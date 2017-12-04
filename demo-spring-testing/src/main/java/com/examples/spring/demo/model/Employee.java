010e8f947973ee633efdf6fdc77f7f8f19a6a031
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-04 12:52:29 +0100

    created initial EmployeeService

---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/model/Employee.java
@@ -0,0 +1,33 @@
+package com.examples.spring.demo.model;
+
+public class Employee {
+
+	private long id;
+	private String name;
+	private long salary;
+
+	public Employee(long id, String name, long salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.examples.spring.demo.repositories;
+
+import java.util.List;
+
+import com.examples.spring.demo.model.Employee;
+
+public interface EmployeeRepository {
+
+	List<Employee> findAll();
+
+}
