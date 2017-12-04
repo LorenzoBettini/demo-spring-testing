b2b3e7ace79b84d2fa8ea09195a843f79d7b28c4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-04 11:44:04 +0100

    initial

---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/DemoSpringTestingApplication.java
@@ -0,0 +1,12 @@
+package com.examples.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringTestingApplication.class, args);
+	}
+}
