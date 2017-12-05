45c9a6c4e09cff72f529b9a7d64e0c0c091b729b
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-05 14:06:21 +0100

    empty implementation of EmployeeRestController and EmployeeService

---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
@@ -0,0 +1,8 @@
+package com.examples.spring.demo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeRestController {
+
+}
---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.examples.spring.demo.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.examples.spring.demo.model.Employee;
+
+@Service
+public class EmployeeService {
+
+	public List<Employee> getAllEmployees() {
+		return null;
+	}
+
+	public Employee getEmployeeById(long i) {
+		return null;
+	}
+
+}
