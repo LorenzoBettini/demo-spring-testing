b3f50d9095dc4c3d6035a4c4c018485f24708a9a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-05 15:00:33 +0100

    modified Employee so that it works with Jpa

--- demo-spring-testing/src/main/java/com/examples/spring/demo/model/Employee.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/model/Employee.java
@@ -1,10 +1,23 @@
 package com.examples.spring.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+	@Id @GeneratedValue
 	private Long id;
 	private String name;
 	private long salary;
+
+	/**
+	 * Required by Jpa
+	 */
+	public Employee() {
+		
+	}
 
 	public Employee(Long id, String name, long salary) {
 		this.id = id;
