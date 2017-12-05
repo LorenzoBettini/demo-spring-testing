a51c243c2c718cecaf50b2c6ff6cf3e5f493b848
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-05 16:01:29 +0100

    other repository methods

--- demo-spring-testing/src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
@@ -1,9 +1,17 @@
 package com.examples.spring.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.examples.spring.demo.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	Employee findByName(String string);
+
+	List<Employee> findByNameAndSalary(String string, long i);
+
+	List<Employee> findByNameOrSalary(String string, long l);
 
 }
