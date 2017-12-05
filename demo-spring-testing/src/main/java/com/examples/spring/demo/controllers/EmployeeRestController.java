962ef8bf488d482b6aa1f0080f8d87121de52016
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-05 14:08:48 +0100

    implemented EmployeeRestController

--- demo-spring-testing/src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
@@ -1,8 +1,29 @@
 package com.examples.spring.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.examples.spring.demo.model.Employee;
+import com.examples.spring.demo.services.EmployeeService;
 
 @RestController
 public class EmployeeRestController {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@GetMapping("/api/employees")
+	public List<Employee> allEmployees() {
+		return employeeService.getAllEmployees();
+	}
+
+	@GetMapping("/api/employees/{id}")
+	public Employee oneEmployee(@PathVariable long id) {
+		return employeeService.getEmployeeById(id);
+	}
 
 }
