0d50ee84f0ebfadc356fd3d349377e266a300673
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-07 11:50:06 +0100

    implemented minimal web controller

---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/controllers/EmployeeWebController.java
@@ -0,0 +1,13 @@
+package com.examples.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeWebController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
