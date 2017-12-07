031b153701693aa6cb54c83e724ac85a786eeb7a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-07 12:03:51 +0100

    web controller delegates to service

--- demo-spring-testing/src/main/java/com/examples/spring/demo/controllers/EmployeeWebController.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/controllers/EmployeeWebController.java
@@ -1,13 +1,27 @@
 package com.examples.spring.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.examples.spring.demo.model.Employee;
+import com.examples.spring.demo.services.EmployeeService;
 
 @Controller
 public class EmployeeWebController {
 
+	private EmployeeService employeeService;
+
+	@Autowired
+	public EmployeeWebController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
 	@GetMapping("/")
 	public String index() {
+		List<Employee> employees = employeeService.getAllEmployees();
 		return "index";
 	}
 }
