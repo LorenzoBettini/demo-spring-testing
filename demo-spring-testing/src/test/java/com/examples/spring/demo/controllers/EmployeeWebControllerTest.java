742cab080852011835127f59dd1cdd08442dbe73
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-07 11:54:23 +0100

    testReturnHomeView

--- demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerTest.java
+++ demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
-
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = EmployeeWebController.class)
@@ -24,4 +24,10 @@ public class EmployeeWebControllerTest {
 			.andExpect(status().is2xxSuccessful());
 	}
 
+	@Test
+	public void testReturnHomeView() throws Exception {
+		ModelAndViewAssert.assertViewName(mvc.perform(get("/"))
+			.andReturn()
+			.getModelAndView(), "index");
+	}
 }
