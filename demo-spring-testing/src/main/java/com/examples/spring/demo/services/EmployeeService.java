99cd782abe658f172ba815b8538e403d653a567a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-07 12:01:47 +0100

    created EmployeeService

---
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.examples.spring.demo.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.examples.spring.demo.model.Employee;
+
+@Service
+public class EmployeeService {
+
+	public List<Employee> getAllEmployees() {
+		return null;
+	}
+
+}
