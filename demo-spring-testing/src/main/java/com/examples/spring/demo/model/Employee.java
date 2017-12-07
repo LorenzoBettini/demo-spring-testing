f61fbdf3e5952c7571a69e9d36fbb59bf74d983a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-07 14:07:55 +0100

    fixed Employee (setters, no-arg cons, equals)

--- demo-spring-testing/src/main/java/com/examples/spring/demo/model/Employee.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/model/Employee.java
@@ -6,6 +6,10 @@ public class Employee {
 	private String name;
 	private long salary;
 
+	public Employee() {
+		// requested for serialization/deserialization
+	}
+
 	public Employee(long id, String name, long salary) {
 		this.id = id;
 		this.name = name;
@@ -24,9 +28,52 @@ public class Employee {
 		return salary;
 	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + (int) (salary ^ (salary >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (salary != other.salary)
+			return false;
+		return true;
 	}
 
 }
