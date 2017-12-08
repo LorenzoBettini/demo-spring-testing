2a35ebcadbb167ca2e896acae01e6da1c6e85246
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-08 20:09:42 +0100

    first test on html table

--- demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
+++ demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
@@ -1,6 +1,10 @@
 package com.examples.spring.demo.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +13,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.examples.spring.demo.model.Employee;
 import com.examples.spring.demo.services.EmployeeService;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = EmployeeWebController.class)
@@ -31,4 +39,16 @@ public class EmployeeWebControllerHtmlUnitTest {
 			.getTextContent()).contains("No employee");
 	}
 
+	@Test
+	public void testHomePageWithEmployees() throws Exception {
+		when(employeeService.getAllEmployees())
+			.thenReturn(
+				Arrays.asList(
+					new Employee(1, "test1", 1000),
+					new Employee(2, "test2", 2000)));
+		HtmlPage page = this.webClient.getPage("/");
+		assertThat(page.getBody().getTextContent())
+			.doesNotContain("No employee");
+		HtmlTable table = page.getHtmlElementById("employee_table");
+	}
 }
