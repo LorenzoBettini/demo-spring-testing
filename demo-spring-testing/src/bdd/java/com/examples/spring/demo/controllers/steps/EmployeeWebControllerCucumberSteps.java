42e2eba6c1de873c6e9c31b459af5a4bf32503ac
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-13 09:24:47 +0100

    implemented other steps as pending

--- demo-spring-testing/src/bdd/java/com/examples/spring/demo/controllers/steps/EmployeeWebControllerCucumberSteps.java
+++ demo-spring-testing/src/bdd/java/com/examples/spring/demo/controllers/steps/EmployeeWebControllerCucumberSteps.java
@@ -2,12 +2,24 @@ package com.examples.spring.demo.controllers.steps;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class EmployeeWebControllerCucumberSteps {
 
 	@Given("^The database is empty$")
 	public void the_database_is_empty() throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^The User is on Home Page$")
+	public void theUserIsOnHomePage() throws Throwable {
+		throw new PendingException();
+	}
+
+	@Then("^A message \"([^\"]*)\" must be shown$")
+	public void aMessageMustBeShown(String arg1) throws Throwable {
 		throw new PendingException();
 	}
 }
