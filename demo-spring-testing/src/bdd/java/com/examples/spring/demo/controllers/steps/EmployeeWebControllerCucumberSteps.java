6a179efea1a8a5af5f2c1351dcda6f45c70e5420
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-12-13 09:12:51 +0100

    created the step implementation file with the pending @Given

---
+++ demo-spring-testing/src/bdd/java/com/examples/spring/demo/controllers/steps/EmployeeWebControllerCucumberSteps.java
@@ -0,0 +1,13 @@
+package com.examples.spring.demo.controllers.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+public class EmployeeWebControllerCucumberSteps {
+
+	@Given("^The database is empty$")
+	public void the_database_is_empty() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+}
