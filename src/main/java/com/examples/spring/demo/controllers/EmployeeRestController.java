f53e0bcbddb0463759221d5460b0f14f4e7cbc6f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-12 18:16:05 +0200

    implementation of GET one

--- src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
+++ src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examples.spring.demo.model.Employee;
@@ -18,5 +19,10 @@ public class EmployeeRestController {
 	@GetMapping("/api/employees")
 	public List<Employee> allEmployees() {
 		return employeeService.getAllEmployees();
+	}
+
+	@GetMapping("/api/employees/{id}")
+	public Employee oneEmployee(@PathVariable long id) {
+		return employeeService.getEmployeeById(id);
 	}
 }
