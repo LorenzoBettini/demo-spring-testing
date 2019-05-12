9237c80983bb5216d339924565cbf0b3064b8c34
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-12 20:01:15 +0200

    refactored REST annotations

--- src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
+++ src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
@@ -5,23 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examples.spring.demo.model.Employee;
 import com.examples.spring.demo.services.EmployeeService;
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeRestController {
 
 	@Autowired
 	private EmployeeService employeeService;
 
-	@GetMapping("/api/employees")
+	@GetMapping
 	public List<Employee> allEmployees() {
 		return employeeService.getAllEmployees();
 	}
 
-	@GetMapping("/api/employees/{id}")
+	@GetMapping("/{id}")
 	public Employee oneEmployee(@PathVariable long id) {
 		return employeeService.getEmployeeById(id);
 	}
