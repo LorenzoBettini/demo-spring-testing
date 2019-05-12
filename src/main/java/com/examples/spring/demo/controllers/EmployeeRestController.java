ec4542b0525858400ebcebf440e1e00d7c0921f4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-12 21:15:35 +0200

    implementation of POST and PUT

--- src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
+++ src/main/java/com/examples/spring/demo/controllers/EmployeeRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,15 @@ public class EmployeeRestController {
 	public Employee oneEmployee(@PathVariable long id) {
 		return employeeService.getEmployeeById(id);
 	}
+
+	@PostMapping("/new")
+	public Employee newEmployee(@RequestBody Employee employee) {
+		return employeeService.insertNewEmployee(employee);
+	}
+
+	@PutMapping("/update/{id}")
+	public Employee updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
+		return employeeService.updateEmployeeById(id, employee);
+	}
+
 }
