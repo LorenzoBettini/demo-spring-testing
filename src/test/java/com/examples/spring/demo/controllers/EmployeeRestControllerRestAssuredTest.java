9e4c85c346879c42879f109e6b3da0119d240b4f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-12 21:15:07 +0200

    failing test for POST and PUT

--- src/test/java/com/examples/spring/demo/controllers/EmployeeRestControllerRestAssuredTest.java
+++ src/test/java/com/examples/spring/demo/controllers/EmployeeRestControllerRestAssuredTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
 
 import com.examples.spring.demo.model.Employee;
 import com.examples.spring.demo.services.EmployeeService;
@@ -33,6 +34,8 @@ public class EmployeeRestControllerRestAssuredTest {
 
 	@Test
 	public void testFindByIdWithExistingEmployee() throws Exception {
+		// this test is also in EmployeeRestControllerTest
+		// this is the version with REST Assured
 		when(employeeService.getEmployeeById(1)).
 			thenReturn(new Employee(1L, "first", 1000));
 
@@ -49,5 +52,45 @@ public class EmployeeRestControllerRestAssuredTest {
 			);
 
 		verify(employeeService, times(1)).getEmployeeById(1);
+	}
+
+	@Test
+	public void testPostEmployee() throws Exception {
+		Employee requestBodyEmployee = new Employee(null, "test", 1000);
+		when(employeeService.insertNewEmployee(requestBodyEmployee)).
+			thenReturn(new Employee(1L, "test", 1000));
+
+		given().
+			contentType(MediaType.APPLICATION_JSON_VALUE).
+			body(requestBodyEmployee).
+		when().
+			post("/api/employees/new").
+		then().
+			statusCode(200).
+			body(
+				"id", equalTo(1),
+				"name", equalTo("test"),
+				"salary", equalTo(1000)
+			);
+	}
+
+	@Test
+	public void testUpdateEmployee() throws Exception {
+		Employee requestBodyEmployee = new Employee(null, "test", 1000);
+		when(employeeService.updateEmployeeById(1L, requestBodyEmployee)).
+			thenReturn(new Employee(1L, "test", 1000));
+
+		given().
+			contentType(MediaType.APPLICATION_JSON_VALUE).
+			body(requestBodyEmployee).
+		when().
+			put("/api/employees/update/1").
+		then().
+			statusCode(200).
+			body(
+				"id", equalTo(1),
+				"name", equalTo("test"),
+				"salary", equalTo(1000)
+			);
 	}
 }
