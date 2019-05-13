d3b798e716c93ef62f607cdb1e8a695be9762914
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-13 16:13:12 +0200

    example of complex query

--- src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.examples.spring.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.examples.spring.demo.model.Employee;
 
@@ -13,5 +15,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	List<Employee> findByNameAndSalary(String string, long i);
 
 	List<Employee> findByNameOrSalary(String string, long l);
+
+	@Query("Select e from Employee e where e.salary < :threshold")
+	List<Employee> findAllEmployeesWithLowSalary(@Param("threshold") long threshold);
 
 }
