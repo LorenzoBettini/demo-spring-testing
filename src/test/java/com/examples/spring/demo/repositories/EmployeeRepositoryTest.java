04ed53f3484cbf29341ca814c365a347bcf5e04a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-13 15:43:53 +0200

    first failing test for repository

---
+++ src/test/java/com/examples/spring/demo/repositories/EmployeeRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.examples.spring.demo.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.examples.spring.demo.model.Employee;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class EmployeeRepositoryTest {
+
+	@Autowired
+	private EmployeeRepository repository;
+
+	@Test
+	public void firstLearningTest() {
+		Employee employee = new Employee(null, "test", 1000);
+		Employee saved = repository.save(employee);
+		Collection<Employee> employees = repository.findAll();
+		assertThat(employees).containsExactly(saved);
+	}
+
+}
