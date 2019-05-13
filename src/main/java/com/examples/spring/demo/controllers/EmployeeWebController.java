cdfee2a5af69f2c25e9c44eef298df35dbf2bafe
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-13 17:02:10 +0200

    made the test pass delegating to service

--- src/main/java/com/examples/spring/demo/controllers/EmployeeWebController.java
+++ src/main/java/com/examples/spring/demo/controllers/EmployeeWebController.java
@@ -1,13 +1,22 @@
 package com.examples.spring.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.examples.spring.demo.services.EmployeeService;
 
 @Controller
 public class EmployeeWebController {
 
+	@Autowired
+	private EmployeeService employeeService;
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("employees",
+				employeeService.getAllEmployees());
 		return "index";
 	}
 
