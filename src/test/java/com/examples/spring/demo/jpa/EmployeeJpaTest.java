b53e3c5a8831e57f1e2b77264f03cfb87302875e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-13 15:39:02 +0200

    assert something in the JPA test

--- src/test/java/com/examples/spring/demo/jpa/EmployeeJpaTest.java
+++ src/test/java/com/examples/spring/demo/jpa/EmployeeJpaTest.java
@@ -1,7 +1,10 @@
 package com.examples.spring.demo.jpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -21,6 +24,14 @@ public class EmployeeJpaTest {
 		Employee saved =
 			entityManager.
 				persistFlushFind(new Employee(null, "test", 1000));
+		assertThat(saved.getName()).isEqualTo("test");
+		assertThat(saved.getSalary()).isEqualTo(1000);
+		assertThat(saved.getId()).isNotNull();
+		assertThat(saved.getId()).isGreaterThan(0);
+		// just to see the generated identifier
+		LoggerFactory
+			.getLogger(EmployeeJpaTest.class)
+				.info("Saved: " + saved.toString());
 	}
 
 }
