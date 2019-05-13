6df9fb625aed3932cfe6f082ff962b2d64ba2962
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-13 16:09:23 +0200

    other examples of repository methods

--- src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/examples/spring/demo/repositories/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.examples.spring.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.examples.spring.demo.model.Employee;
@@ -7,5 +9,9 @@ import com.examples.spring.demo.model.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 	Employee findByName(String string);
+
+	List<Employee> findByNameAndSalary(String string, long i);
+
+	List<Employee> findByNameOrSalary(String string, long l);
 
 }
