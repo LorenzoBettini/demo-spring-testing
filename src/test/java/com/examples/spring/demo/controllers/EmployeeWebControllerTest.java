8cd3b8b3566b62243cba85e6200d221225c302be
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-13 16:21:57 +0200

    first failing test for web

---
+++ src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerTest.java
@@ -0,0 +1,27 @@
+package com.examples.spring.demo.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = EmployeeWebController.class)
+public class EmployeeWebControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testStatus200() throws Exception {
+		mvc.perform(get("/"))
+			.andExpect(status().is2xxSuccessful());
+	}
+
+}
