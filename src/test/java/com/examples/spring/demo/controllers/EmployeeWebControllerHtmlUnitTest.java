f90c00d586d7ed55dc71ea5174b4d2aa6ecf02de
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-14 16:20:04 +0200

    failing test for editing existing employee

--- src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
+++ src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
@@ -3,7 +3,7 @@ package com.examples.spring.demo.controllers;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.examples.spring.demo.model.Employee;
 import com.examples.spring.demo.services.EmployeeService;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 
@@ -75,6 +76,29 @@ public class EmployeeWebControllerHtmlUnitTest {
 
 		assertThat(page.getBody().getTextContent())
 			.contains("No employee found with id: 1");
+	}
+
+	@Test
+	public void testEditExistentEmployee() throws Exception {
+		when(employeeService.getEmployeeById(1))
+			.thenReturn(new Employee(1L, "original name", 1000));
+
+		HtmlPage page = this.webClient.getPage("/edit/1");
+
+		// Get the form that we are dealing with
+		final HtmlForm form = page.getFormByName("employee_form");
+		// make sure the fields are filled with the correct values
+		// and then change their values
+		form.getInputByValue("original name").setValueAttribute("modified name");
+		form.getInputByValue("1000").setValueAttribute("2000");
+
+		// Now submit the form by clicking the button and get back the second page.
+		form.getButtonByName("btn_submit").click();
+
+		// verify that the modified employee has been updated through the service
+		// using the values entered in the form
+		verify(employeeService)
+			.updateEmployeeById(1L, new Employee(1L, "modified name", 2000));
 	}
 
 	private String removeWindowsCR(String s) {
