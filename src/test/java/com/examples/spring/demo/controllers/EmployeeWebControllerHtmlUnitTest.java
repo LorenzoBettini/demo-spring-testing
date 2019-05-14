a98a67366e911c42281222ef9404a0a25512920f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-14 09:34:10 +0200

    failing test for "No employee"

--- src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
+++ src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
@@ -1,6 +1,8 @@
 package com.examples.spring.demo.controllers;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +29,17 @@ public class EmployeeWebControllerHtmlUnitTest {
 	public void testHomePageTitle() throws Exception {
 		HtmlPage page = webClient.getPage("/");
 		assertThat(page.getTitleText()).isEqualTo("Employees");
+	}
+
+	@Test
+	public void testHomePageWithNoEmployees() throws Exception {
+		when(employeeService.getAllEmployees())
+			.thenReturn(emptyList());
+
+		HtmlPage page = this.webClient.getPage("/");
+
+		assertThat(page.getBody()
+			.getTextContent()).contains("No employee");
 	}
 
 }
