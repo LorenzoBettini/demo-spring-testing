8c56c2167dbdc704fc4adc6d1b140224394b2088
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-14 09:00:52 +0200

    first failing test for index title

---
+++ src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
@@ -0,0 +1,32 @@
+package com.examples.spring.demo.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.examples.spring.demo.services.EmployeeService;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = EmployeeWebController.class)
+public class EmployeeWebControllerHtmlUnitTest {
+
+	@Autowired
+	private WebClient webClient;
+
+	@MockBean
+	private EmployeeService employeeService;
+
+	@Test
+	public void testHomePageTitle() throws Exception {
+		HtmlPage page = webClient.getPage("/");
+		assertThat(page.getTitleText()).isEqualTo("Employees");
+	}
+
+}
